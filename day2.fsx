11715819bc3453b5ccea261ab2e845cdbfec05b9
Lari Tuomisto <lari.tuomisto@solita.fi>
2024-12-02 15:46:24 +0200

    but wait, there's more

--- day2.fsx
+++ day2.fsx
@@ -1,7 +1,7 @@
 open System.IO
 
 let generatePermutations (nums: int[]) = [| for i in 0 .. nums.Length - 1 -> Array.append (nums.[..i-1]) (nums.[i+1..]) |]
-let isOrdered (seq) = seq |> Array.pairwise |> Array.forall (fun (a, b) -> a > b && abs (a-b) <= 3)
+let isOrdered = Array.pairwise >> Array.forall (fun (a, b) -> a > b && abs (a-b) <= 3)
 let stage1 = File.ReadAllLines "inputs/day2" |> Array.map (fun line -> line.Split() |> Array.map int)
 
 let answer1 = stage1 |> Array.collect (fun row -> [| row; Array.rev row |]) |> Array.filter isOrdered |> Array.length
