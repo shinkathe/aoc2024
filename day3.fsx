ee1d9a806bec29d5aa23633a26ded3d5af9d44b0
Lari Tuomisto <lari.tuomisto@solita.fi>
2024-12-03 12:46:49 +0200

    I'm back

--- day3.fsx
+++ day3.fsx
@@ -2,23 +2,10 @@ open System.IO
 open System.Text.RegularExpressions
 
 let stage1 = File.ReadAllText "inputs/day3"
+let cleanedInput = Regex.Replace(stage1, @"don't\(\).*?do\(\)", "", RegexOptions.Singleline)
+let instructionPattern = @"mul\((\d+),(\d+)\)"
+let answer1Matches = Regex.Matches(stage1, instructionPattern)
+let answer2Matches = Regex.Matches(cleanedInput, instructionPattern)
 
-let findInstructions skip input =
-    let pattern = @"do\(\)|don't\(\)|mul\((\d+),(\d+)\)"
-    let matches = Regex(pattern).Matches(input)
-    let mutable isEnabled = true
-    let mutable enabledMulInstructions = []
-    for m in matches do
-        let instruction = m.Value
-        match instruction with
-        | "do()" ->
-            isEnabled <- skip || true
-        | "don't()" ->
-            isEnabled <- skip || false
-        | _ when instruction.StartsWith("mul") && isEnabled ->
-            enabledMulInstructions <- (int m.Groups.[1].Value, int m.Groups.[2].Value) :: enabledMulInstructions
-        | _ -> ()
-    enabledMulInstructions
-
-printfn "Answer 1: %d" (findInstructions true stage1 |> List.sumBy (fun (n1, n2) -> n1 * n2))
-printfn "Answer 2: %d" (findInstructions false stage1 |> List.sumBy (fun (n1, n2) -> n1 * n2))
+printfn "Answer1: %d" <| (answer1Matches |> Seq.map (fun m -> int m.Groups.[1].Value * int m.Groups.[2].Value )|> Seq.sum)
+printfn "Answer1: %d" <| (answer2Matches |> Seq.map (fun m -> int m.Groups.[1].Value * int m.Groups.[2].Value )|> Seq.sum)
