feb705d8be1f621618c7773e4a2f2a55d0fc4ce5
Lari Tuomisto <lari.tuomisto@solita.fi>
2024-12-03 12:48:26 +0200

    Simpler

--- day3.fsx
+++ day3.fsx
@@ -7,5 +7,5 @@ let instructionPattern = @"mul\((\d+),(\d+)\)"
 let answer1Matches = Regex.Matches(stage1, instructionPattern)
 let answer2Matches = Regex.Matches(cleanedInput, instructionPattern)
 
-printfn "Answer1: %d" <| (answer1Matches |> Seq.map (fun m -> int m.Groups.[1].Value * int m.Groups.[2].Value )|> Seq.sum)
-printfn "Answer2: %d" <| (answer2Matches |> Seq.map (fun m -> int m.Groups.[1].Value * int m.Groups.[2].Value )|> Seq.sum)
+printfn "Answer1: %d" <| (answer1Matches |> Seq.sumBy (fun m -> int m.Groups.[1].Value * int m.Groups.[2].Value ))
+printfn "Answer2: %d" <| (answer2Matches |> Seq.sumBy (fun m -> int m.Groups.[1].Value * int m.Groups.[2].Value ))
