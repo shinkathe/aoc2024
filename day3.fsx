6514c80da408fa9542c103926745d49cad6be492
Lari Tuomisto <lari.tuomisto@solita.fi>
2024-12-03 13:26:11 +0200

    golf

--- day3.fsx
+++ day3.fsx
@@ -1,11 +1,10 @@
 open System.IO
 open System.Text.RegularExpressions
 
-let stage1 = File.ReadAllText "inputs/day3"
-let cleanedInput = Regex.Replace(stage1, @"don't\(\).*?do\(\)", "", RegexOptions.Singleline)
+let input = File.ReadAllText "inputs/day3"
 let instructionPattern = @"mul\((\d+),(\d+)\)"
-let answer1Matches = Regex.Matches(stage1, instructionPattern)
-let answer2Matches = Regex.Matches(cleanedInput, instructionPattern)
+let answer1Matches = Regex.Matches(input, instructionPattern)
+let answer2Matches = Regex.Matches(Regex.Replace(input, @"don't\(\).*?do\(\)", "", RegexOptions.Singleline), instructionPattern)
 
 printfn "Answer1: %d" <| (answer1Matches |> Seq.sumBy (fun m -> int m.Groups.[1].Value * int m.Groups.[2].Value ))
 printfn "Answer2: %d" <| (answer2Matches |> Seq.sumBy (fun m -> int m.Groups.[1].Value * int m.Groups.[2].Value ))
