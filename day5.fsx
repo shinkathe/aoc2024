310b827631deec3413465e973d9deb38f7e2e7bb
Lari Tuomisto <lari.tuomisto@solita.fi>
2024-12-05 17:02:12 +0200

    Refac

--- day5.fsx
+++ day5.fsx
@@ -1,8 +1,9 @@
-open System.IO
+open System
 
-let stage1 = File.ReadAllLines("inputs/day5")
-let orderingRules = stage1 |> Array.filter (fun line -> line.Contains("|")) |> Array.map (fun line -> line.Split('|') |> fun parts -> (int parts.[0], int parts.[1]))
-let lines = stage1 |> Array.filter (fun line -> line.Contains(",")) |> Array.map (fun line -> line.Split(',') |> Array.map int)
+let input = IO.File.ReadAllText "inputs/day5" |> _.Split("\n\n")
+let orderingRules = input |> Array.head |> _.Split("\n") |> Array.map (fun line -> line.Split('|') |> fun p -> int p[0], int p[1])
+let lines = input |> Array.last |> _.Split("\n") |> Array.map (fun line -> line.Split(',') |> Array.map int)
+
 let getMiddleElement (array:int[]) = array.[array.Length / 2]
 
 let rec swap (a:int[]) rules =
@@ -16,8 +17,8 @@ let rec swap (a:int[]) rules =
 
 let swappedLines = lines |> Array.map (fun line -> (line, swap line orderingRules))
 
-let answer1 = swappedLines |> Array.filter (fun (original, swapped) -> original = swapped) |> Array.map (fun (original, _) -> getMiddleElement original) |> Array.sum
-let answer2 = swappedLines |> Array.filter (fun (original, swapped) -> original <> swapped) |> Array.map (fun (_, swapped) -> getMiddleElement swapped) |> Array.sum
+let answer1 = swappedLines |> Array.filter (fun (original, swapped) -> original = swapped) |> Array.map (fst >> getMiddleElement) |> Array.sum
+let answer2 = swappedLines |> Array.filter (fun (original, swapped) -> original <> swapped) |> Array.map (snd >> getMiddleElement) |> Array.sum
 
 printfn "Answer 1: %A" answer1
 printfn "Answer 2: %A" answer2
